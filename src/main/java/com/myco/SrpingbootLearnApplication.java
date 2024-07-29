021f7ed523db13835989ecd8d66d9e7faec9aefb
Melroy <melroyr@yahoo.com>
2024-07-29 17:11:03 -0400

    Initial checkin

---
+++ src/main/java/com/myco/SrpingbootLearnApplication.java
@@ -0,0 +1,13 @@
+package com.myco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingbootLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myco/SrpingbootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingbootLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
